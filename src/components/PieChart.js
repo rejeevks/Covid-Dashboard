71926bf0993b2a8a111196b3c831da90cb6b8314
rejeevks <rejeevks14@gmail.com>
2021-11-02 12:33:32 +0530

    chart

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,33 +1,33 @@
-// import React from 'react';
-// import Plot from 'react-plotly.js';
+import React from "react";
+import Plot from "react-plotly.js";
 
-// class PieChart extends React.Component {
-//   render() {
-//     return (
-//       <Plot
-//         data={[
-//           {
-//             x: [1, 2, 3],
-//             y: [2, 6, 3],
-//             type: 'scatter',
-//             mode: 'lines+markers',
-//             marker: {color: 'red'},
-//           },
-//           {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-//         ]}
-//         layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
-//       />
-//     );
-//   }
-// }
-// export default PieChart
-
-// import React from 'react'
-
-// const PieChart=(props)=>{
-//     return (
-//        <div>PieChart</div>
-//     )
-// }
-
-// export default PieChart
+class PieChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [{
+        values: [19, 26, 55],
+        labels: ['Residential', 'Non-Residential', 'Utility'],
+        type: 'pie'
+      }],
+      layout: {
+        height: 400,
+        width: 500,
+        title: "Pie chart"
+      }
+    };
+  }
+  render() {
+    return (
+      <div style={{ width: "100%", height: "100%" }}>
+        <Plot
+          data={this.state.data}
+          layout={this.state.layout}
+          onInitialized={(figure) => this.setState(figure)}
+          onUpdate={(figure) => this.setState(figure)}
+        />
+      </div>
+    );
+  }
+}
+export default PieChart;
