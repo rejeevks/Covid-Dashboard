7296996f489414d5f325f99df2a608d0c3ceeb97
rejeevks <rejeevks14@gmail.com>
2021-11-02 13:57:49 +0530

    chart

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,33 +1,22 @@
 import React from "react";
 import Plot from "react-plotly.js";
 
-class PieChart extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [{
-        values: [20,19, 26, 55],
-        labels: ['Total Cases','Active Cases', 'Total Recovered', 'Total Deaths'],
-        type: 'pie'
-      }],
-      layout: {
-        height: 400,
-        width: 500,
-        title: "Pie chart"
-      }
-    };
-  }
-  render() {
-    return (
-      <div style={{ width: "100%", height: "100%" }}>
-        <Plot
-          data={this.state.data}
-          layout={this.state.layout}
-          onInitialized={(figure) => this.setState(figure)}
-          onUpdate={(figure) => this.setState(figure)}
-        />
-      </div>
-    );
-  }
+
+const PieChart = (props) =>{
+  const {tcases}=props;
+  console.log("tt",tcases);
+  var data = [
+    {
+      values: [tcases, 4454, 65555, 44],
+      labels: ["Total Cases", "Active Cases", "Total Recovered", "Total Deaths"],
+      type: "pie",
+    },
+  ];
+
+    return(
+      <Plot
+      data={data}
+      layout={ {width: 500, height: 400, title: 'Covid Details '} } />
+    )
 }
 export default PieChart;
