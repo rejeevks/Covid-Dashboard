6b88c6b6f8d61eac984d44bd3e9c591317a37415
rejeevks <rejeevks14@gmail.com>
2021-11-17 16:53:59 +0530

    chart fix

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,22 +1,32 @@
 import React from "react";
 import Plot from "react-plotly.js";
 
-
-const PieChart = (props) =>{
-  const {tcases}=props;
-  console.log("tt",tcases);
-  var data = [
-    {
-      values: [tcases, 4454, 65555, 44],
-      labels: ["Total Cases", "Active Cases", "Total Recovered", "Total Deaths"],
-      type: "pie",
-    },
-  ];
-
-    return(
-      <Plot
+const PieChart = (props) => {
+  const { cases } = props;
+  if (cases.length > 0) {
+    var data = [
+      {
+        values: [
+          cases[0].confirmed,
+          cases[0].active,
+          cases[0].recovered,
+          cases[0].deaths,
+        ],
+        labels: [
+          "Total Cases",
+          "Active Cases",
+          "Total Recovered",
+          "Total Deaths",
+        ],
+        type: "pie",
+      },
+    ];
+  }
+  return (
+    <Plot
       data={data}
-      layout={ {width: 500, height: 400, title: 'Covid Details '} } />
-    )
-}
+      layout={{ width: 500, height: 400, title: "Covid Details " }}
+    />
+  );
+};
 export default PieChart;
